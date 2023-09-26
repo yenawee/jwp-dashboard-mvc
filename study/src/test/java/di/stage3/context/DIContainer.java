5cef90c5c1848005ba83fb256ca8caa1f9273d41
yenawee <yaena0319@naver.com>
2023-09-26 14:08:52 +0900

    feat: DI 3단계 학습테스트

--- study/src/test/java/di/stage3/context/DIContainer.java
+++ study/src/test/java/di/stage3/context/DIContainer.java
@@ -1,6 +1,12 @@
 package di.stage3.context;
 
+import di.ConsumerWrapper;
+import di.FunctionWrapper;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
@@ -10,11 +16,35 @@ class DIContainer {
     private final Set<Object> beans;
 
     public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+        this.beans = classes.stream()
+                .map(FunctionWrapper.apply(Class::getDeclaredConstructor))
+                .peek(constructor -> constructor.setAccessible(true))
+                .map(FunctionWrapper.apply(Constructor::newInstance))
+                .collect(Collectors.toUnmodifiableSet());
+        this.beans.forEach(this::setFields);
+    }
+
+    private void setFields(Object bean) {
+        for (var field : bean.getClass().getDeclaredFields()) {
+            setFields(bean, field);
+        }
+    }
+
+    private void setFields(Object bean, Field field) {
+        var fieldType = field.getType();
+        field.setAccessible(true);
+
+        beans.stream()
+                .filter(fieldType::isInstance)
+                .forEach(ConsumerWrapper.accept(matchedField -> field.set(bean, matchedField)));
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
-        return null;
+        return beans.stream()
+                .filter(aClass::isInstance)
+                .findFirst()
+                .map(bean -> (T) bean)
+                .orElse(null);
     }
 }
